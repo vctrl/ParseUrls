6a4c0554de71a04af4553c098a0b3f34a617316b
Viktor Shutov <vectoreal@gmail.com>
2021-01-23 04:24:30 +0300

    create page parser struct

--- main.go
+++ main.go
@@ -19,6 +19,65 @@ type URLData struct {
 	Categories []string `json:"categories"`
 }
 
+type PageParser struct {
+	reader io.Reader
+	url    string
+}
+
+func (p *PageParser) GetPageData() ([]byte, error) {
+	title, err := p.getPageTagValue([]byte("title"))
+	description, err := p.getPageTagValue([]byte("description"))
+
+	if err != nil {
+		return nil, err
+	}
+
+	var buf bytes.Buffer
+
+	buf.WriteString(p.url)
+	buf.Write([]byte("\t"))
+	buf.Write(title)
+	buf.Write([]byte("\t"))
+	buf.Write(description)
+
+	return buf.Bytes(), nil
+}
+
+func (p *PageParser) getPageTagValue(tag []byte) ([]byte, error) {
+	dataInBytes, err := ioutil.ReadAll(p.reader)
+	if err != nil {
+		return nil, err
+	}
+
+	pageContent := dataInBytes
+
+	// Find a substr
+	titleStartIndex := bytes.Index(pageContent, []byte("<title>"))
+	if titleStartIndex == -1 {
+		fmt.Println("No title element found")
+		return nil, fmt.Errorf("error parsing url %s: no title element found", p.url)
+	}
+
+	titleStartIndex += len(tag) + 2
+
+	// Find the index of the closing tag
+	titleEndIndex := bytes.Index(pageContent, []byte("</title>"))
+
+	if titleEndIndex == -1 {
+		fmt.Println("No closing tag for title found.")
+		return nil, fmt.Errorf("error parsing url %s: no closing tag for title found", p.url)
+	}
+
+	if titleStartIndex >= len(pageContent) ||
+		titleEndIndex >= len(pageContent) ||
+		titleEndIndex < titleStartIndex {
+		return nil, fmt.Errorf("error parsing url %s, title tag start index %d, title tag end index %d, page length%d",
+			p.url, titleStartIndex, titleEndIndex, len(pageContent))
+	}
+
+	return pageContent[titleStartIndex:titleEndIndex], nil
+}
+
 func main() {
 	f, err := os.Open("500.jsonl")
 	if err != nil {
@@ -27,6 +86,7 @@ func main() {
 
 	downloadURLs, err := NewWorkerPool(500)
 	saveToFile, err := NewWorkerPool(500)
+
 	if err != nil {
 		log.Fatalf("error while starting worker pool: %v", err)
 	}
@@ -64,7 +124,8 @@ func main() {
 				return
 			}
 
-			pageTitle, err := getPageTitle(response.Body, urlData.URL)
+			pageParser := &PageParser{reader: response.Body, url: urlData.URL}
+			pageData, err := pageParser.GetPageData()
 
 			if err != nil {
 				fmt.Printf("error parsing page:%v", err.Error())
@@ -81,7 +142,7 @@ func main() {
 
 			for _, category := range categories {
 				createChannelIfNotExist(dataChByCategory, category, saveToFile, mu)
-				dataChByCategory[category] <- pageTitle
+				dataChByCategory[category] <- pageData
 			}
 		})
 	}
@@ -96,6 +157,18 @@ func main() {
 
 	downloadURLs.Stop()
 	saveToFile.Stop()
+}
+
+func createChannelIfNotExist(dataChByCategory map[string](chan []byte), category string, wp *WorkerPool, mu *sync.Mutex) {
+	if _, ok := dataChByCategory[category]; !ok {
+		ch := make(chan []byte, 100)
+		category := category
+
+		wp.AddTask(func() { listen(category, ch) })
+		mu.Lock()
+		dataChByCategory[category] = ch
+		mu.Unlock()
+	}
 }
 
 func listen(category string, subscriber chan []byte) {
@@ -122,51 +195,4 @@ func listen(category string, subscriber chan []byte) {
 	}
 
 	f.Write(buf.Bytes())
-}
-
-func getPageTitle(reader io.Reader, url string) ([]byte, error) {
-	dataInBytes, err := ioutil.ReadAll(reader)
-	if err != nil {
-		return nil, err
-	}
-
-	pageContent := dataInBytes
-
-	// Find a substr
-	titleStartIndex := bytes.Index(pageContent, []byte("<title>"))
-	if titleStartIndex == -1 {
-		fmt.Println("No title element found")
-		return nil, fmt.Errorf("error parsing url %s: no title element found", url)
-	}
-
-	titleStartIndex += 7
-
-	// Find the index of the closing tag
-	titleEndIndex := bytes.Index(pageContent, []byte("</title>"))
-
-	if titleEndIndex == -1 {
-		fmt.Println("No closing tag for title found.")
-		return nil, fmt.Errorf("error parsing url %s: no closing tag for title found", url)
-	}
-
-	if titleStartIndex >= len(pageContent) ||
-		titleEndIndex >= len(pageContent) ||
-		titleEndIndex < titleStartIndex {
-		return nil, fmt.Errorf("error parsing url %s, title tag start index %d, title tag end index %d, page length%d",
-			url, titleStartIndex, titleEndIndex, len(pageContent))
-	}
-
-	return pageContent[titleStartIndex:titleEndIndex], nil
-}
-
-func createChannelIfNotExist(dataChByCategory map[string](chan []byte), category string, wp *WorkerPool, mu *sync.Mutex) {
-	if _, ok := dataChByCategory[category]; !ok {
-		ch := make(chan []byte, 100)
-		category := category
-
-		wp.AddTask(func() { listen(category, ch) })
-		mu.Lock()
-		dataChByCategory[category] = ch
-		mu.Unlock()
-	}
 }
