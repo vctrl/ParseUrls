cdbc449fddabb12c750f7733d85145d7e4fc272c
Viktor Shutov <vectoreal@gmail.com>
2021-01-26 11:37:08 +0300

    refactor and tests

--- main.go
+++ main.go
@@ -24,6 +24,10 @@ type PageParser struct {
 	url    string
 }
 
+type TaskAdder interface {
+	AddTask(func()) error
+}
+
 type HttpClient interface {
 	Get(url string) (resp *http.Response, err error)
 }
@@ -40,7 +44,7 @@ func (p *PageParser) GetPageData() ([]byte, error) {
 	description, err := p.getDescription(dataInBytes)
 
 	if err != nil {
-		fmt.Println(err)
+		// fmt.Println(err)
 	}
 
 	var buf bytes.Buffer
@@ -104,20 +108,138 @@ func (p *PageParser) getPageTagValue(pageContent []byte, openingTag []byte, clos
 	return pageContent[tagStartIndex:tagEndIndex], nil
 }
 
+type FileBatchWriter struct {
+	// File       io.WriteCloser
+	Data       chan []byte
+	BatchSize  int
+	Category   string
+	SaveToFile TaskAdder
+	OpenFile   OpenFileFunc
+}
+
+type OpenWriter interface {
+	Write(f io.WriteCloser, data chan []byte, category string)
+	Query(f io.WriteCloser, data chan []byte, category string) error
+	Open(name string, flag int, perm os.FileMode) (io.WriteCloser, error)
+}
+
+func NewFileBatchWriter(wp *WorkerPool, batchSize int,
+	openFile OpenFileFunc) *FileBatchWriter {
+	return &FileBatchWriter{SaveToFile: wp, BatchSize: batchSize, OpenFile: openFile}
+}
+
+func (fbw *FileBatchWriter) Write(f io.WriteCloser, data chan []byte, category string) {
+	var buf bytes.Buffer
+	i := 0
+
+	defer f.Close()
+
+	for message := range data {
+		fmt.Printf("%q: %q\n", category, message)
+		buf.Write(message)
+		buf.Write([]byte("\n"))
+
+		i++
+		if i%fbw.BatchSize == 0 {
+			f.Write(buf.Bytes())
+			buf.Reset()
+		}
+	}
+
+	f.Write(buf.Bytes())
+}
+
+func (fbw *FileBatchWriter) Open(name string, flag int, perm os.FileMode) (io.WriteCloser, error) {
+	f, err := fbw.OpenFile(name, flag, perm)
+	if err != nil {
+		return nil, err
+	}
+
+	return f, nil
+}
+
+func (fbw *FileBatchWriter) Query(f io.WriteCloser, ch chan []byte, category string) error {
+	return fbw.SaveToFile.AddTask(func() {
+		fbw.Write(f, ch, category)
+	})
+}
+
+type URLDownloader struct {
+	Client           HttpClient
+	mu               *sync.Mutex
+	dataChByCategory map[string](chan []byte)
+}
+
+func (downloader *URLDownloader) Download(input *URLData) ([]byte, error) {
+	response, err := downloader.Client.Get(input.URL)
+	if response != nil {
+		defer response.Body.Close()
+	}
+
+	if err != nil {
+		return nil, err
+	}
+
+	pageParser := &PageParser{reader: response.Body, url: input.URL}
+	pageData, err := pageParser.GetPageData()
+
+	if err != nil {
+		return nil, err
+	}
+
+	return pageData, nil
+}
+
+func (d *URLDownloader) Process(urlData *URLData, fbw OpenWriter) error {
+	fmt.Println("wtf?")
+	pageData, err := d.Download(urlData)
+	if err != nil {
+		return err
+	}
+
+	d.QueryForSaving(urlData.Categories, pageData, fbw)
+	return nil
+}
+
+func (downloader *URLDownloader) QueryForSaving(categories []string, pageData []byte, fbw OpenWriter) {
+	if len(categories) == 0 {
+		categories = []string{"empty"}
+	}
+
+	for _, category := range categories {
+		if _, ok := downloader.dataChByCategory[category]; !ok {
+			ch := make(chan []byte, 500)
+
+			f, err := fbw.Open(fmt.Sprintf("results/%s.tsv", category), os.O_WRONLY|os.O_CREATE, 0666)
+			if err != nil {
+				fmt.Printf("error opening file: %v", err)
+				continue
+			}
+
+			fbw.Query(f, ch, category)
+
+			downloader.mu.Lock()
+			downloader.dataChByCategory[category] = ch
+			downloader.mu.Unlock()
+		}
+
+		downloader.dataChByCategory[category] <- pageData
+	}
+}
+
 func main() {
-	f, err := os.Open("500.jsonl")
+	file, err := os.Open("500.jsonl")
 	if err != nil {
 		log.Fatalf("error while reading file: %v", err)
 	}
 
+	parseURLFromFile, err := NewWorkerPool(500)
 	downloadURLs, err := NewWorkerPool(500)
 	saveToFile, err := NewWorkerPool(500)
 
 	if err != nil {
 		log.Fatalf("error while starting worker pool: %v", err)
 	}
-
-	dataChByCategory := make(map[string](chan []byte))
 
 	tr := &http.Transport{
 		DisableKeepAlives:  true,
@@ -127,110 +249,76 @@ func main() {
 
 	client := &http.Client{Timeout: time.Second * 30, Transport: tr}
 
-	openFile := func(name string, flag int, perm os.FileMode) (io.WriteCloser, error) {
-		return os.OpenFile(name, os.O_WRONLY|os.O_CREATE, 0666)
-	}
+	URLDownloader := &URLDownloader{Client: client,
+		dataChByCategory: make(map[string](chan []byte)),
+		mu:               &sync.Mutex{}}
 
-	parseURLs(f, downloadURLs, saveToFile, dataChByCategory, client, openFile)
+	fbw := NewFileBatchWriter(saveToFile, 20, OpenFileFunc(func(name string, flag int, perm os.FileMode) (io.WriteCloser, error) {
+		return os.OpenFile(name, flag, perm)
+	}))
+
+	process(file,
+		parseURLFromFile, downloadURLs, saveToFile,
+		URLDownloader, fbw)
 }
 
-func parseURLs(f io.Reader,
+func process(f io.Reader,
+	parseURLFromFile *WorkerPool,
 	downloadURLs *WorkerPool,
 	saveToFile *WorkerPool,
-	dataChByCategory map[string](chan []byte),
-	client HttpClient,
-	openFile OpenFileFunc) {
-	mu := &sync.Mutex{}
-	s := bufio.NewScanner(f)
-	for s.Scan() {
-		var urlData URLData
-		err := json.Unmarshal(s.Bytes(), &urlData)
-		if err != nil {
-			fmt.Printf("unmarshal error:%v\n", err.Error())
-			continue
-		}
+	urlDownloader *URLDownloader,
+	fbw *FileBatchWriter) {
+	parsed := make(chan *URLData, 500)
+	go parseFile(f, parseURLFromFile, parsed)
 
+	for urlData := range parsed {
 		downloadURLs.AddTask(func() {
-			response, err := client.Get(urlData.URL)
-			if response != nil {
-				defer response.Body.Close()
-			}
-
-			if err != nil {
-				fmt.Printf("http error: %v", err.Error())
-				return
-			}
-
-			pageParser := &PageParser{reader: response.Body, url: urlData.URL}
-			pageData, err := pageParser.GetPageData()
-
-			if err != nil {
-				fmt.Println(err.Error())
-				return
-			}
-
-			var categories []string
-
-			if len(urlData.Categories) == 0 {
-				categories = []string{"empty"}
-			} else {
-				categories = urlData.Categories
-			}
-
-			for _, category := range categories {
-				f, err := openFile(fmt.Sprintf("results/%s.tsv", category), os.O_WRONLY|os.O_CREATE, 0666)
-				if err != nil {
-					fmt.Printf("error opening file: %v", err)
-					continue
-				}
-
-				createChannelIfNotExist(f, dataChByCategory, category, saveToFile, mu)
-				dataChByCategory[category] <- pageData
-			}
+			urlDownloader.Process(urlData, fbw)
+			// fmt.Println(downloadURLs.Wg)
 		})
 	}
 
+	parseURLFromFile.Wait()
+
 	downloadURLs.Wait()
 
-	for _, dataCh := range dataChByCategory {
+	for _, dataCh := range urlDownloader.dataChByCategory {
 		close(dataCh)
 	}
 
 	saveToFile.Wait()
 
-	downloadURLs.Stop()
 	saveToFile.Stop()
+	downloadURLs.Stop()
+	parseURLFromFile.Stop()
+	fmt.Println("done")
 }
 
-func createChannelIfNotExist(f io.WriteCloser, dataChByCategory map[string](chan []byte), category string, wp *WorkerPool, mu *sync.Mutex) {
-	if _, ok := dataChByCategory[category]; !ok {
-		ch := make(chan []byte, 100)
-		category := category
+func parseFile(f io.Reader, wp TaskAdder, result chan *URLData) {
+	s := bufio.NewScanner(f)
+	wg1 := &sync.WaitGroup{}
+	for s.Scan() {
+		wg1.Add(1)
+		wg := &sync.WaitGroup{}
+		wg.Add(1)
+		wp.AddTask(func() {
+			var urlData URLData
+			err := json.Unmarshal(s.Bytes(), &urlData)
+			wg.Done()
+			if err != nil {
+				wg1.Done()
+				fmt.Printf("unmarshal error:%v\n", err.Error())
+				return
+			}
 
-		wp.AddTask(func() { listen(f, category, ch) })
-		mu.Lock()
-		dataChByCategory[category] = ch
-		mu.Unlock()
-	}
-}
+			result <- &urlData
+			wg1.Done()
+			// fmt.Println("done", wg1)
+		})
 
-func listen(f io.WriteCloser, category string, subscriber chan []byte) {
-	var buf bytes.Buffer
-	i := 0
-
-	defer f.Close()
-
-	for message := range subscriber {
-		fmt.Printf("%q: %q\n", category, message)
-		buf.Write(message)
-		buf.Write([]byte("\n"))
-
-		i++
-		if i%20 == 0 {
-			f.Write(buf.Bytes())
-			buf.Reset()
-		}
+		wg.Wait()
 	}
 
-	f.Write(buf.Bytes())
+	wg1.Wait()
+	close(result)
 }
--- main_test.go
+++ main_test.go
@@ -2,12 +2,16 @@ package main
 
 import (
 	"bytes"
+	"fmt"
 	"io"
 	"io/ioutil"
 	"net/http"
 	"os"
+	"reflect"
 	"strings"
+	"sync"
 	"testing"
+	"time"
 )
 
 var (
@@ -54,6 +58,14 @@ type FileMock struct {
 	content []byte
 }
 
+type WorkerPoolMock struct {
+}
+
+func (*WorkerPoolMock) AddTask(callback func()) error {
+	callback()
+	return nil
+}
+
 func (f *FileMock) Write(p []byte) (n int, err error) {
 	t := make([]byte, len(p))
 	f.content = append(f.content, t...)
@@ -62,6 +74,21 @@ func (f *FileMock) Write(p []byte) (n int, err error) {
 }
 
 func (*FileMock) Close() error {
+	return nil
+}
+
+type FileBatchWriterMock struct {
+}
+
+func (fbw *FileBatchWriterMock) Write(f io.WriteCloser, data chan []byte, category string) {
+
+}
+
+func (fbw *FileBatchWriterMock) Open(name string, flag int, perm os.FileMode) (io.WriteCloser, error) {
+	return nil, nil
+}
+
+func (fbw *FileBatchWriterMock) Query(f io.WriteCloser, ch chan []byte, category string) error {
 	return nil
 }
 
@@ -75,23 +102,134 @@ func TestGetPageData(t *testing.T) {
 	}
 }
 
+func TestParseFile(t *testing.T) {
+	reader := strings.NewReader(file)
+	result := make(chan *URLData, 10)
+	parseFile(reader, &WorkerPoolMock{}, result)
+
+	expected := &URLData{URL: "http://test.url", Categories: []string{"test_category"}}
+	fact := <-result
+	if !reflect.DeepEqual(expected, fact) {
+		t.Errorf("expected %v, got %v", expected, fact)
+	}
+}
+
+func TestGroupByCategories(t *testing.T) {
+	category1 := "test_category1"
+	category2 := "test_category2"
+
+	result := make(map[string](chan []byte))
+	client := &HttpClientMock{}
+	urlDownloader := &URLDownloader{Client: client, mu: &sync.Mutex{}, dataChByCategory: result}
+	input1 := &URLData{URL: "http://test.url", Categories: []string{category1}}
+	urlDownloader.Process(input1, &FileBatchWriterMock{})
+	input2 := &URLData{URL: "http://test.url", Categories: []string{category1}}
+	urlDownloader.Process(input2, &FileBatchWriterMock{})
+	input3 := &URLData{URL: "http://test.url", Categories: []string{category2}}
+	urlDownloader.Process(input3, &FileBatchWriterMock{})
+
+	for _, ch := range result {
+		close(ch)
+	}
+
+	i := 0
+	for range result[category1] {
+		i++
+	}
+
+	if i != 2 {
+		t.Errorf("expected 2, but found %d in %s", i, category1)
+	}
+
+	i = 0
+	for range result["test_category2"] {
+		i++
+	}
+
+	if i != 1 {
+		t.Errorf("expected 1, but found %d in %s", i, category2)
+	}
+}
+
+func TestBatchWriter(t *testing.T) {
+	f := &FileMock{}
+
+	input := make(chan []byte, 10)
+	fbw := &FileBatchWriter{BatchSize: 5, Category: "test_category", SaveToFile: &WorkerPoolMock{}}
+
+	// wg for first 4 messages
+	wg := sync.WaitGroup{}
+
+	// wg1 for the last message
+	wg1 := sync.WaitGroup{}
+	wg1.Add(1)
+
+	wg.Add(4)
+	go func() {
+		for i := 0; i < 4; i++ {
+			input <- []byte(fmt.Sprintf("test_message %d", i))
+			wg.Done()
+		}
+
+		time.Sleep(time.Second * 2)
+
+		input <- []byte(fmt.Sprintf("test message 4"))
+		time.Sleep(time.Second)
+		wg1.Done()
+	}()
+
+	go func() {
+		fbw.Write(f, input, "test_category")
+	}()
+
+	wg.Wait()
+
+	// nothing
+	if len(f.content) > 0 {
+		t.Errorf("file should be empty, but was %v", f.content)
+	}
+
+	wg1.Wait()
+
+	expected := []byte("test_message 0\ntest_message 1\ntest_message 2\ntest_message 3\ntest message 4\n")
+
+	// one more message
+	if !reflect.DeepEqual(f.content, expected) {
+		t.Errorf("expected: %v, but was: %v", f.content, expected)
+	}
+
+	close(input)
+}
+
 func TestParseURLs(t *testing.T) {
 	reader := strings.NewReader(file)
+
+	parseURLFromFile, err := NewWorkerPool(10)
 	downloadURLs, err := NewWorkerPool(10)
 	saveToFile, err := NewWorkerPool(10)
+
 	if err != nil {
 		t.Fatal("can't create worker pools")
 	}
 
-	dataChByCategory := make(map[string](chan []byte))
 	client := &HttpClientMock{}
 
 	fileMock := &FileMock{content: make([]byte, 0)}
+
+	URLDownloader := &URLDownloader{Client: client,
+		dataChByCategory: make(map[string](chan []byte)),
+		mu:               &sync.Mutex{},
+	}
+
 	openFileMock := func(name string, flag int, perm os.FileMode) (io.WriteCloser, error) {
 		return fileMock, nil
 	}
 
-	parseURLs(reader, downloadURLs, saveToFile, dataChByCategory, client, openFileMock)
+	fbw := NewFileBatchWriter(saveToFile, 20, openFileMock)
+
+	process(reader,
+		parseURLFromFile, downloadURLs, saveToFile,
+		URLDownloader, fbw)
 
 	expected := []byte("http://test.url\ttest title\ttest description\n")
 	if bytes.Compare(fileMock.content, expected) != 0 {
